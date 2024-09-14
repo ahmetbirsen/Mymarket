b8dc90dc4626dea135978360ddb525e351e97fcb
ahmetbirsen <ahmetbrsn44@hotmail.com>
2024-09-14 18:02:06 +0300

    data, domain and presentation created.

--- app/src/main/java/com/example/mymarket/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/mymarket/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.mymarket.ui.theme
+package com.example.mymarket.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/mymarket/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/mymarket/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.mymarket.ui.theme
+package com.example.mymarket.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
